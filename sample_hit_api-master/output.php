2f830248facec6cb0fc7e24df335aa86745bcc29
annisardona <45420990+annisardona@users.noreply.github.com>
2019-06-12 14:54:26 +0700

    Add files via upload

---
+++ sample_hit_api-master/output.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="<?php echo base_url() ?>assets/images/logo-javawebmedia.png" rel="shortcut icon">
+<title><?php echo $title ?></title>
+<link href="<?php echo base_url() ?>assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<section class="login">
+	<h1>Login Page</h1>
+    <form action="<?php echo site_url('sample_hit/prettyPrint') ?>" method="post">
+    
+     <p>
+        <label for="idPel">ID Pelanggan</label>
+        <input type="text" name="idPel" id="idPel" <?php echo $data['trx_id'] ?>>
+        
+      </p>
+	  <p>
+        <label for="idPel">ID Pelanggan</label>
+        <input type="text" name="idPel" id="idPel" <?php echo $data['subscriber_id'] ?>>
+        
+      </p>
+	  <p>
+        <label for="idPel">ID Pelanggan</label>
+        <input type="text" name="idPel" id="idPel" <?php echo $data['subscriber_name'] ?>>
+        
+      </p>
+      <p>
+        <input type="submit" name="kirim" id="kirim" value="Kirim" class="full">
+      </p>
+    </form>
+    <footer>Web design by <a href="http://javawebmedia.com" title="School of graphic &amp; web design" target="_blank">Java Web Media</a></footer>
+</section>
+</body>
+</html>
